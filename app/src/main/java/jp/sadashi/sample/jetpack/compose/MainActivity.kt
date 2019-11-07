b294fa80cb2ebeca14579aeedf6c21b4dd2e51e3
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-11-07 18:07:13 +0900

    Jetpack Compose Codelab.

--- app/src/main/java/jp/sadashi/sample/jetpack/compose/MainActivity.kt
+++ app/src/main/java/jp/sadashi/sample/jetpack/compose/MainActivity.kt
@@ -3,31 +3,97 @@ package jp.sadashi.sample.jetpack.compose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.Model
+import androidx.compose.unaryPlus
+import androidx.ui.core.CurrentTextStyleProvider
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.layout.CrossAxisAlignment
+import androidx.ui.layout.ExpandedHeight
+import androidx.ui.layout.Spacing
+import androidx.ui.material.*
+import androidx.ui.material.surface.Surface
+import androidx.ui.text.TextStyle
 import androidx.ui.tooling.preview.Preview
+
+// Simplified version of a typical AppState
+class AppState(val counterState: CounterState = CounterState())
+
+@Model
+class CounterState(var count: Int = 0)
+
+val customGreen = Color(0xFF1EB980.toInt())
+val customSurface = Color(0xFF26282F.toInt())
+private val themeColors = MaterialColors(
+        primary = customGreen,
+        surface = customSurface,
+        onSurface = Color.Black
+)
+
+@Composable
+fun CustomTheme(children: @Composable() () -> Unit) {
+    MaterialTheme(colors = themeColors) {
+        val textStyle = TextStyle(color = Color.Red)
+        CurrentTextStyleProvider(value = textStyle) {
+            children()
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            MyApp { MyScreenContent() }
         }
     }
 }
 
 @Composable
+fun MyApp(child: @Composable() () -> Unit) {
+    CustomTheme {
+        Surface(color = Color.Yellow) {
+            child()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(appState: AppState = AppState()) {
+    Column(modifier = ExpandedHeight, crossAxisAlignment = CrossAxisAlignment.Center) {
+        Column(modifier = Flexible(1f), crossAxisAlignment = CrossAxisAlignment.Center) {
+            Greeting("Android")
+            Divider(color = Color.Black)
+            Greeting("there")
+        }
+        Counter(appState.counterState)
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+            text = "Hello $name!",
+            modifier = Spacing(24.dp),
+            style = +themeTextStyle { h1 }
+    )
+}
+
+@Composable
+fun Counter(state: CounterState) {
+    Button(
+            text = "I've been clicked ${state.count} times",
+            onClick = {
+                state.count++
+            }
+    )
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
+    MyApp { MyScreenContent() }
 }
